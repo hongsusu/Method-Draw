a90e6fb2aafb366fd95c83986f90a82c9534fd39
honghaifeng <18500893514@163.com>
2019-04-22 17:33:09 +0800

    merhod draw

---
+++ src/dragupload.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  debugger
+  document.querySelector('body').addEventListener('drop', function(e) {
+    e.preventDefault();
+    var reader = new FileReader();
+    reader.onload = function(evt) {
+      //document.querySelector('img').src = evt.target.result;
+    };
+
+    reader.readAsDataURL(e.dataTransfer.files[0]);
+  }, false);
+}
